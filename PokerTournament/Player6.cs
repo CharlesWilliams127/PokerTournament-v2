cd532a6d2b45ff2a772a6edd7b124648f4e2bc1d
Charles Williams <crw4147@rit.edu>
2017-04-03 18:21:44 -0400

    Initial push of Project 3

---
+++ PokerTournament/Player6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerTournament
+{
+    class Player6 : Player
+    {
+        public Player6(int idNum, string nm, int mny) : base(idNum, nm, mny)
+        {
+        }
+
+        public override PlayerAction BettingRound1(List<PlayerAction> actions, Card[] hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PlayerAction BettingRound2(List<PlayerAction> actions, Card[] hand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override PlayerAction Draw(Card[] hand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
