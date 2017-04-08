4ac12bb0c7917e9a1783d2b96e21853dc932e55e
Steve Dill <sdd4163@rit.edu>
2017-04-08 17:48:57 -0400

    1) Began framework of Discarding logic (unfunctional currently)

--- PokerTournament/Player6.cs
+++ PokerTournament/Player6.cs
@@ -8,9 +8,14 @@ namespace PokerTournament
 {
     class Player6 : Player
     {
+        int enemyFirstBet;
+
         public Player6(int idNum, string nm, int mny) : base(idNum, nm, mny)
         {
+            enemyFirstBet = 0;
         }
+
+        //Probabilities: http://www.math.hawaii.edu/~ramsey/Probability/PokerHands.html
 
         public override PlayerAction BettingRound1(List<PlayerAction> actions, Card[] hand)
         {
@@ -24,7 +29,126 @@ namespace PokerTournament
 
         public override PlayerAction Draw(Card[] hand)
         {
-            throw new NotImplementedException();
+            // determine how many cards to delete
+            int numCardsToDelete = 0;
+            int[] cardsToDelete;
+            Card curHighCard;
+            int curHandStrength = Evaluate.RateAHand(hand, out curHighCard);
+            bool takeASmallRisk = false;
+            bool takeABigRisk = false;
+
+            if (enemyFirstBet > 250 && enemyFirstBet < 500)
+            {
+                takeASmallRisk = true;
+            } else if (enemyFirstBet > 500)
+            {
+                takeASmallRisk = true;
+                takeABigRisk = true;
+            }
+
+            switch (curHandStrength)
+            {
+                case 1:
+                    numCardsToDelete = 4;
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    numCardsToDelete = 0;
+                    break;
+                case 9:
+                    numCardsToDelete = 0;
+                    break;
+                case 10:
+                    numCardsToDelete = 0;
+                    break;
+            }
+
+            PlayerAction pa = null;
+            if (curHandStrength > 5)
+            {
+                if (curHandStrength >= 8)
+                {
+                    pa = new PlayerAction(Name, "Draw", "stand pat", 0);
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+
+            }
+
+            cardsToDelete = new int[numCardsToDelete];
+
+            // which cards to delete if any
+            if (numCardsToDelete > 0 && numCardsToDelete < 5)
+            {
+                for (int i = 0; i < numCardsToDelete; i++) // loop to delete cards
+                {
+                    Console.WriteLine("\nDelete card " + (i + 1) + ":");
+                    for (int j = 0; j < hand.Length; j++)
+                    {
+                        Console.WriteLine("{0} - {1}", (j + 1), hand[j]);
+                    }
+                    // selete cards to delete
+                    int delete = 0;
+                    do
+                    {
+
+                        Console.Write("Which card to delete? (1 - 5): ");
+                        string delStr = Console.ReadLine();
+                        int.TryParse(delStr, out delete);
+
+                        // see if the entry is valid
+                        if (delete < 1 || delete > 5)
+                        {
+                            Console.WriteLine("Invalid entry - enter a value between 1 and 5.");
+                            delete = 0;
+                        }
+                        else if (hand[delete - 1] == null)
+                        {
+                            Console.WriteLine("Entry was already deleted.");
+                            delete = 0;
+                        }
+                        else
+                        {
+                            hand[delete - 1] = null; // delete entry
+                            delete = 99; // flag to exit loop
+                        }
+                    } while (delete == 0);
+                }
+                // set the PlayerAction object
+                pa = new PlayerAction(Name, "Draw", "draw", numCardsToDelete);
+            }
+            else if (numCardsToDelete == 5)
+            {
+                // delete them all
+                for (int i = 0; i < hand.Length; i++)
+                {
+                    hand[i] = null;
+                }
+                pa = new PlayerAction(Name, "Draw", "draw", 5);
+            }
+            else // no cards deleted
+            {
+                pa = new PlayerAction(Name, "Draw", "stand pat", 0);
+            }
+
+            // return the action
+            return pa;
         }
     }
 }
