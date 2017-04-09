95010d1831dd6fac941e8ba513d77d50225ef53a
WarpedTime <silverstorm79@gmail.com>
2017-04-08 20:23:34 -0400

    base

--- PokerTournament/Player6.cs
+++ PokerTournament/Player6.cs
@@ -9,26 +9,106 @@ namespace PokerTournament
     class Player6 : Player
     {
         int enemyFirstBet;
+        int roundNum;
+        int startingMoney;
+        int lastActionStr;
+        int lastActionAmount;
 
         public Player6(int idNum, string nm, int mny) : base(idNum, nm, mny)
         {
             enemyFirstBet = 0;
+            roundNum = 0;
+            startingMoney = mny;
         }
 
         //Probabilities: http://www.math.hawaii.edu/~ramsey/Probability/PokerHands.html
 
         public override PlayerAction BettingRound1(List<PlayerAction> actions, Card[] hand)
         {
-            throw new NotImplementedException();
+            //info to keep track of
+            bool isFirst = false;
+            
+            // evaluate the hand
+            Card highCard = null;
+            int rank = Evaluate.RateAHand(hand, out highCard);
+
+            //update stats
+            if (actions.Count > 0) //ai goes second
+            {
+                Console.WriteLine("\n*round stats-> First Player: " + actions[0].Name + " total turns taken: " + actions.Count);
+                for (int i = 0; i < actions.Count; i++)
+                {
+                    Console.WriteLine("   (" + (i) + "): PLR: " + actions[i].Name + ", action: " + actions[i].ActionName + ", inPhase: " + actions[i].ActionPhase + ", amount: " + actions[i].Amount);
+                }
+            }
+            else //ai goes first
+            {
+                Console.WriteLine("\n*round stats-> player 1: " + Name + " takes first turn of round.");
+                isFirst = true;
+            }
+
+            //setup action
+            string actionSelection = "1";
+            PlayerAction pa = null;
+            int amount = 20;
+
+            //start turn
+            Console.WriteLine("\n-> in ai betting round 1");
+            Console.WriteLine("   Total games played: "+roundNum);
+
+            //if ai is first
+            if (isFirst)
+            {
+
+            }else
+            {
+
+            }
+
+
+
+            //end turn and submit action
+            //create the PlayerAction
+            switch (actionSelection)
+            {
+                case "1": pa = new PlayerAction(Name, "Bet1", "bet", amount); break;
+                case "2": pa = new PlayerAction(Name, "Bet1", "raise", amount); break;
+                case "3": pa = new PlayerAction(Name, "Bet1", "call", amount); break;
+                case "4": pa = new PlayerAction(Name, "Bet1", "check", amount); break;
+                case "5": pa = new PlayerAction(Name, "Bet1", "fold", amount); break;
+                default: Console.WriteLine("Invalid menu selection - try again"); break;
+            } Console.WriteLine("< end ai betting round 1 >");
+            return pa;
         }
 
         public override PlayerAction BettingRound2(List<PlayerAction> actions, Card[] hand)
         {
-            throw new NotImplementedException();
+            // select an action
+            string actionSelection = "5";
+            PlayerAction pa = null;
+            int amount = 0;
+
+            Console.WriteLine("-> in ai betting round 2");
+
+            // create the PlayerAction
+            switch (actionSelection)
+            {
+                case "1": pa = new PlayerAction(Name, "Bet2", "bet", amount); break;
+                case "2": pa = new PlayerAction(Name, "Bet2", "raise", amount); break;
+                case "3": pa = new PlayerAction(Name, "Bet2", "call", amount); break;
+                case "4": pa = new PlayerAction(Name, "Bet2", "check", amount); break;
+                case "5": pa = new PlayerAction(Name, "Bet2", "fold", amount); break;
+                default: Console.WriteLine("Invalid menu selection - try again"); break;
+            } Console.WriteLine("< end ai betting round 2 >");
+
+            roundNum++;
+            return pa;
         }
 
         public override PlayerAction Draw(Card[] hand)
         {
+            Console.WriteLine("-> in ai draw round");
+
             // determine how many cards to delete
             int numCardsToDelete = 0;
             int[] cardsToDelete;
--- PokerTournament/Program.cs
+++ PokerTournament/Program.cs
@@ -29,10 +29,11 @@ namespace PokerTournament
         {
             // create two players
             Human h0 = new Human(0, "Joe", 1000);
-            Human h1 = new Human(1, "Sue", 1000);
+            //Human h1 = new Human(1, "Sue", 1000);
+            Player6 ai = new Player6(1, "AI", 1000);
 
             // create the Game
-            Game myGame = new Game(h0, h1);
+            Game myGame = new Game(h0, ai);
 
             myGame.Tournament(); // run the game
         }
